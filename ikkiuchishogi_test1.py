49f9e17c3c5d5da7a39b85513d46bd22163b1d4b
unknown <yakinoki>
2021-12-09 23:38:07 +0900

    tebann_change

--- ikkiuchishogi_test1.py
+++ ikkiuchishogi_test1.py
@@ -3,9 +3,10 @@ import os
 
 os.makedirs('./result_kifu',exist_ok=True)
 
+
 class ShogiCls:
-	def __init__(self):
-		self.shogi_bit = list()
+	def __init__(self) -> None:
+		self.shogi_bit = list() 
 		# 初期設定＝盤上に王と玉を置く。
 		for i in range(81):
 			if i == 4:   
@@ -22,11 +23,8 @@ class ShogiCls:
 		self.tebann = config['sennte']
 
 
-	
-   
-
 	# 盤面を表示する。
-	def shogi_display(self):
+	def shogi_display(self) -> None:
 		print("Ｘ１ ２ ３ ４ ５ ６ ７ ８ ９")
 		for l in range(1,10):
 			print("{}".format(l), end='' )
@@ -36,26 +34,34 @@ class ShogiCls:
 
 
 	# 手番表示。
-	def shogi_yourturn(self):
+	def shogi_yourturn(self) -> None:
 		if self.tebann == '王':
 			print("先手の手番です。")
 		else:
 			print("後手の手番です。")   
 	
 
-	
+	# 手番交換。
+	def shogi_tebann_change(self) -> None:
+		if self.tebann == '王':
+			self.tebann = '玉'
+		else:
+			self.tebann = '王'
+
+
 	# 駒を動かす位置を入力して指定。
-	def shogi_inputXY(self):
+	def shogi_inputXY(self)-> None:
 		while True:
 			print("1から9までの整数を1つ入力して下さい。")
 			myXY = input()
 			if myXY == "":
 				continue
 			if myXY.isdigit() and (1 <= int(myXY) <= 9): 
+				taikyoku.shogi_tebann_change()
+				taikyoku.shogi_yourturn()
 				return int(myXY)
-			
-			
 
+	
 
 if __name__ == '__main__':
 
