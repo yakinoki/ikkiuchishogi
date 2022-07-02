509056f5fbdeea7af5e4b06c6030925a910ba7f4
unknown <yakinoki>
2022-07-02 11:05:21 +0900

    update_code

--- ikkiuchishogi_game.py
+++ ikkiuchishogi_game.py
@@ -108,10 +108,12 @@ if __name__ == '__main__':
     # 手番表示。
     taikyoku.shogi_yourturn()
 
+    # 対局終了までループ。
     while True:
-        
         # 対局終了時
         if taikyoku.is_tsumi():
-            # どこに駒を移動させるか入力。
-            taikyoku.shogi_inputXY()
+            break
+        
+        # どこに駒を移動させるか入力。
+        taikyoku.shogi_inputXY()
         
