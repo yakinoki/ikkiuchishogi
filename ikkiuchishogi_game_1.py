3de815fc63d7a3e2415408e1a96196e27c6a855f
unknown <yakinoki>
2022-07-16 22:34:11 +0900

    20220716

---
+++ ikkiuchishogi_game_1.py
@@ -0,0 +1,156 @@
+import yaml
+import os
+import logging
+from logging import getLogger, StreamHandler, Formatter
+
+os.makedirs('./result_kifu',exist_ok=True)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class ShogiCls:
+    def __init__(self, tesuu=0) -> None:
+
+        # 初期設定＝盤上に王と玉を置く。
+        self.shogi_bit = list() 
+        for i in range(81):
+            if i == 4:   
+                self.shogi_bit.append('玉')
+            elif i == 76:
+                self.shogi_bit.append('王')
+            else:
+                self.shogi_bit.append('・')
+        self.tesuu = tesuu
+
+        #手番。対局開始時は先手。 
+        with open('config.yml','r',encoding="utf-8") as yml:
+            config = yaml.safe_load(yml)       
+        self.tebann = config['sennte']
+
+    # 盤面を表示する。
+    def shogi_display(self) -> None:
+        print("Ｘ１ ２ ３ ４ ５ ６ ７ ８ ９")
+        for l in range(1,10):
+            print("{}".format(l), end='' )
+            for c in range(1,10):
+                print(" {}".format(self.shogi_bit[(l-1)*9+(c-1)]), end='')
+            print()
+
+    # 手番表示。
+    def shogi_yourturn(self) -> None:
+        if self.tebann == '王':
+            logger.info("先手の手番です。")
+        else:
+            logger.info("後手の手番です。")   
+    
+
+    # 手番交換。
+    def shogi_tebann_change(self) -> None:
+        if self.tebann == '王':
+            self.tebann = '玉'
+        else:
+            self.tebann = '王'
+
+    # 駒を動かす位置を入力して指定。
+    def shogi_inputXY(self)-> None:
+        while True:
+            if self.tebann == '王':
+                for i in range(1,80):
+                    if self.shogi_bit[i] == '王':
+                        self.ou = i
+                        break
+                    else:
+                        i = i + 1
+                print(self.ou)        
+                logger.info("先手の手番です。横方向の移動を指定するために1から9までの整数を1つ入力して下さい。")
+                myXY = input()
+                if myXY == "":
+                    continue
+                if myXY.isdigit(): 
+                    x = int(myXY)-1
+                    self.shogi_bit[self.ou] = '・'
+                    self.shogi_bit[x + 9*int((self.ou)/9)] = '王'
+                    taikyoku.shogi_display()
+                for i in range(1,80):
+                    if self.shogi_bit[i] == '王':
+                        self.ou = i
+                        break
+                    else:
+                        i = i + 1
+                print(self.ou)
+                logger.info("縦横方向の移動を指定するために1から9までの整数を1つ入力して下さい。")
+                myXY = input()
+                if myXY == "":
+                    continue
+                if myXY.isdigit(): 
+                    x = 9 * (int(myXY)-1) + self.ou  % 9
+                    print(x)
+                    self.shogi_bit[self.ou] = '・'
+                    self.shogi_bit[x] = '王'
+                    print(self.ou)
+                    taikyoku.shogi_display()
+                    taikyoku.shogi_tebann_change()
+                    taikyoku.shogi_yourturn()
+                    return int(myXY)
+                    return True
+            else:
+                for i in range(1,80):
+                    if self.shogi_bit[i] == '玉':
+                        self.gyoku = i
+                        break
+                    else:
+                        i = i + 1
+                print(self.gyoku)
+                logger.info("後手の手番です。横方向の移動を指定するために1から9までの整数を1つ入力して下さい。")
+                myXY = input()
+                if myXY == "":
+                    continue
+                if myXY.isdigit(): 
+                    x = int(myXY)-1
+                    self.shogi_bit[self.gyoku] = '・'
+                    self.shogi_bit[x+ 9*int((self.gyoku)/9)] = '玉'
+                    taikyoku.shogi_display()
+                for i in range(1,80):
+                    if self.shogi_bit[i] == '玉':
+                        self.gyoku = i
+                        break
+                    else:
+                        i = i + 1
+                print(self.gyoku)
+                logger.info("縦横方向の移動を指定するために1から9までの整数を1つ入力して下さい。")
+                myXY = input()
+                if myXY == "":
+                    continue
+                if myXY.isdigit(): 
+                    x = 9 * (int(myXY)-1) + self.gyoku % 9
+                    print(x)
+                    self.shogi_bit[self.gyoku] = '・'
+                    self.shogi_bit[x] = '玉'
+                    taikyoku.shogi_display()
+                    taikyoku.shogi_tebann_change()
+                    taikyoku.shogi_yourturn()
+                    return int(myXY)
+                    return True
+
+   
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    taikyoku = ShogiCls()
+
+    # 盤面表示。
+    taikyoku.shogi_display()
+
+    # 手番表示。
+    taikyoku.shogi_yourturn()
+
+    # 対局終了までループ。
+    while True:
+        
+        # どこに駒を移動させるか入力。
+        taikyoku.shogi_inputXY()
+        
--- ikkiuchishogi_play.py
+++
@@ -1,1 +0,0 @@
-from ikkiuchishogi_game import ShogiCls
